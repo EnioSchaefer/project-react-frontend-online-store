fb07a83c51ee92ab2df6f89a2f189dce799fdfab
Enio Schaefer <enio100schaefer@hotmail.com>
2022-10-12 17:11:43 -0300

    [Fix] :bug: Arrumando importacao indevida no SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { getCategories } from '../services/api';
 
 class SideBar extends React.Component {
